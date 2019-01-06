b4d8c3e104b5311727e3dd922549c63e0b452858
ben willkommen <benwillkommen@gmail.com>
2019-01-05 20:05:54 -0600

    got sheet titles, and individual sheets downloading. commiting before slight cleanup.

--- index.js
+++ index.js
@@ -1,70 +1,25 @@
 const fs = require('fs');
-const readline = require('readline');
 const { google } = require('googleapis');
+const authorize = require('./authorize')
+const { promisify } = require('util');
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
+const SPREADSHEET_ID = process.argv[2] || '1Au638nEKSAa2xl8WucH_XW3tw8urKooXJHr9kmlkf1E'
 
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
+
+    function withAuth(callback, spreadsheetId, auth) {
+        callback(spreadsheetId, auth);
+    }
+
+    const processWithAuth = withAuth.bind(null, downloadSpreadsheetAsync, SPREADSHEET_ID);
+
     // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), listMajors);
+    authorize(JSON.parse(content), processWithAuth);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error while trying to retrieve access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
-}
 
 /**
  * Prints the names and majors of students in a sample spreadsheet:
@@ -90,4 +45,98 @@ function listMajors(auth) {
             console.log('No data found.');
         }
     });
+}
+
+function downloadSpreadsheetAsync(spreadsheetId, auth) {
+    const sheetsClient = google.sheets({ version: 'v4', auth });
+    const getSpreadsheetAsync = promisify(sheetsClient.spreadsheets.get);
+
+    getSpreadsheetAsync({ spreadsheetId })
+        .then(res => res.data.sheets.map(s => s.properties.title))
+        .then(sheetTitles => getSheetsAsync(spreadsheetId, sheetTitles, sheetsClient))
+        .then(sheets => {
+            console.log(sheets)
+        })
+    // .then(sheetTitles => {
+    //     //const sheetTitles = res.data.sheets.map(s => s.properties.title);
+    //     processSheets(spreadsheetId, sheetTitles, sheetsClient);
+    //     return res;
+    // }).then(asdf => {
+    //     console.log(asdf);
+    // });
+}
+
+function getSheetsAsync(spreadsheetId, sheetTitles, sheetsClient) {
+    sheetTitles = sheetTitles.slice(0, 3); // uncomment when ready to process them all
+
+    const getSheetAsync = promisify(sheetsClient.spreadsheets.values.get);
+
+    const sheetPromises = sheetTitles.map(sheetTitle => {
+        return getSheetAsync({
+            spreadsheetId: spreadsheetId,
+            range: sheetTitle,
+        }).then(s => {
+            s.data.sheetTitle = sheetTitle;
+            return s;
+        });
+    }); 
+
+    return Promise.all(sheetPromises);
+}
+
+function downloadSpreadsheet(spreadsheetId, auth) {
+    const sheetsClient = google.sheets({ version: 'v4', auth });
+    sheetsClient.spreadsheets.get({
+        spreadsheetId: spreadsheetId
+    }, (err, res) => {
+        const sheetTitles = res.data.sheets.map(s => s.properties.title);
+        processSheets(spreadsheetId, sheetTitles, sheetsClient);
+    });
+}
+
+function processSheets(spreadsheetId, sheetTitles, sheetsClient) {
+    sheetTitles = sheetTitles.slice(0, 1); // uncomment when ready to process them all
+
+    sheetTitles.map(sheetTitle => {
+        sheetsClient.spreadsheets.values.get({
+            spreadsheetId: spreadsheetId,
+            range: sheetTitle,
+        }, (err, res) => {
+            const rows = res.data.values;
+            if (rows.length) {
+                console.log('Name, Major:');
+                // Print columns A and E, which correspond to indices 0 and 4.
+                rows.map((row) => {
+                    //console.log(`${row[0]}, ${row[4]}`);
+                    console.log(row)
+                });
+            }
+        })
+    })
+
+
+}
+
+function processSheetsAsync(spreadsheetId, sheetTitles, sheetsClient) {
+    sheetTitles = sheetTitles.slice(0, 1); // uncomment when ready to process them all
+
+    sheetTitles.map(sheetTitle => {
+
+        sheetsClient.spreadsheets.values.get({
+            spreadsheetId: spreadsheetId,
+            range: sheetTitle,
+        }, (err, res) => {
+            const rows = res.data.values;
+            if (rows.length) {
+                console.log('Name, Major:');
+                // Print columns A and E, which correspond to indices 0 and 4.
+                rows.map((row) => {
+                    //console.log(`${row[0]}, ${row[4]}`);
+                    console.log(row)
+                });
+            }
+        })
+    })
+
+
 }
