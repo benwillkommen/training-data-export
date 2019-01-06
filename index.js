c254a8fb55996a72acfcbcc7fd3db42dc8e5f081
ben willkommen <benwillkommen@gmail.com>
2019-01-05 20:19:18 -0600

    cleaned up a bit. next step: extract day data from individual sheets

--- index.js
+++ index.js
@@ -19,54 +19,29 @@ fs.readFile('credentials.json', (err, content) => {
     authorize(JSON.parse(content), processWithAuth);
 });
 
-
-
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function listMajors(auth) {
-    const sheets = google.sheets({ version: 'v4', auth });
-    sheets.spreadsheets.values.get({
-        spreadsheetId: '1Au638nEKSAa2xl8WucH_XW3tw8urKooXJHr9kmlkf1E',
-        range: 'Week 150',
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const rows = res.data.values;
-        if (rows.length) {
-            console.log('Name, Major:');
-            // Print columns A and E, which correspond to indices 0 and 4.
-            rows.map((row) => {
-                //console.log(`${row[0]}, ${row[4]}`);
-                console.log(row)
-            });
-        } else {
-            console.log('No data found.');
-        }
-    });
-}
-
 function downloadSpreadsheetAsync(spreadsheetId, auth) {
     const sheetsClient = google.sheets({ version: 'v4', auth });
     const getSpreadsheetAsync = promisify(sheetsClient.spreadsheets.get);
 
     getSpreadsheetAsync({ spreadsheetId })
         .then(res => res.data.sheets.map(s => s.properties.title))
-        .then(sheetTitles => getSheetsAsync(spreadsheetId, sheetTitles, sheetsClient))
-        .then(sheets => {
-            console.log(sheets)
-        })
-    // .then(sheetTitles => {
-    //     //const sheetTitles = res.data.sheets.map(s => s.properties.title);
-    //     processSheets(spreadsheetId, sheetTitles, sheetsClient);
-    //     return res;
-    // }).then(asdf => {
-    //     console.log(asdf);
-    // });
+        .then(sheetTitles => downloadSheetsAsync(spreadsheetId, sheetTitles, sheetsClient))
+        .then(sheetPromises => sheetPromises.map(processSheet));
 }
 
-function getSheetsAsync(spreadsheetId, sheetTitles, sheetsClient) {
+function processSheet(sheetPromise) {
+    sheetPromise.then(sheet => {
+        const rows = sheet.data.values;
+        console.log(sheet.data.sheetTitle);
+
+        for (let i = 0; i < rows.length; i++) {
+            console.log(rows[i])
+        }
+
+    });
+}
+
+function downloadSheetsAsync(spreadsheetId, sheetTitles, sheetsClient) {
     sheetTitles = sheetTitles.slice(0, 3); // uncomment when ready to process them all
 
     const getSheetAsync = promisify(sheetsClient.spreadsheets.values.get);
@@ -79,64 +54,7 @@ function getSheetsAsync(spreadsheetId, sheetTitles, sheetsClient) {
             s.data.sheetTitle = sheetTitle;
             return s;
         });
-    }); 
-
-    return Promise.all(sheetPromises);
-}
-
-function downloadSpreadsheet(spreadsheetId, auth) {
-    const sheetsClient = google.sheets({ version: 'v4', auth });
-    sheetsClient.spreadsheets.get({
-        spreadsheetId: spreadsheetId
-    }, (err, res) => {
-        const sheetTitles = res.data.sheets.map(s => s.properties.title);
-        processSheets(spreadsheetId, sheetTitles, sheetsClient);
     });
-}
 
-function processSheets(spreadsheetId, sheetTitles, sheetsClient) {
-    sheetTitles = sheetTitles.slice(0, 1); // uncomment when ready to process them all
-
-    sheetTitles.map(sheetTitle => {
-        sheetsClient.spreadsheets.values.get({
-            spreadsheetId: spreadsheetId,
-            range: sheetTitle,
-        }, (err, res) => {
-            const rows = res.data.values;
-            if (rows.length) {
-                console.log('Name, Major:');
-                // Print columns A and E, which correspond to indices 0 and 4.
-                rows.map((row) => {
-                    //console.log(`${row[0]}, ${row[4]}`);
-                    console.log(row)
-                });
-            }
-        })
-    })
-
-
-}
-
-function processSheetsAsync(spreadsheetId, sheetTitles, sheetsClient) {
-    sheetTitles = sheetTitles.slice(0, 1); // uncomment when ready to process them all
-
-    sheetTitles.map(sheetTitle => {
-
-        sheetsClient.spreadsheets.values.get({
-            spreadsheetId: spreadsheetId,
-            range: sheetTitle,
-        }, (err, res) => {
-            const rows = res.data.values;
-            if (rows.length) {
-                console.log('Name, Major:');
-                // Print columns A and E, which correspond to indices 0 and 4.
-                rows.map((row) => {
-                    //console.log(`${row[0]}, ${row[4]}`);
-                    console.log(row)
-                });
-            }
-        })
-    })
-
-
+    return sheetPromises;
 }
