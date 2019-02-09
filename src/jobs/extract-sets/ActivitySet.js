db96d00205ce58b48e4aa32d6bbe98b409a8dd02
ben willkommen <benwillkommen@gmail.com>
2019-02-09 11:54:24 -0600

    began moving 'row-cell-processing' to 'extract-set', and related re-structuring/refactoring

---
+++ src/jobs/extract-sets/ActivitySet.js
@@ -0,0 +1,20 @@
+const uuid = require('uuid/v4');
+class ActivitySet {
+    constructor(row, setNumber, strategyUsed, reps, weight, dropSetId) {
+        this.id = uuid();
+        this.week = row.week;
+        this.day = row.day;
+        this.exercise = row.exercise;
+        this.setNumber = setNumber;
+        this.reps = Number(reps) || Number(row.reps);
+        this.instructions = row.instructions;
+        this.weight = Number(weight) || Number(row.weight);
+        this.notes = row.notes;
+        this.supersetId = row.supersetId;
+        this.anomalous = false;
+        this.strategyUsed = strategyUsed,
+            this.dropSetId = dropSetId;
+    }
+}
+
+module.exports = ActivitySet;
