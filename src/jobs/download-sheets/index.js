9ffd44173216f82dc579b08d1f7fcb819d21c45b
ben willkommen <benwillkommen@gmail.com>
2019-02-17 17:38:43 -0600

    added other sheet id in

--- src/jobs/download-sheets/index.js
+++ src/jobs/download-sheets/index.js
@@ -1,6 +1,8 @@
 const db = require('../../db');
 
-const SPREADSHEET_ID = process.argv[2] || '1Au638nEKSAa2xl8WucH_XW3tw8urKooXJHr9kmlkf1E';
+const sheetIds = ['1CEL_GMcq8Y04LpHbsqa7HMFH1tqpXaYAr9Eqjpepuew', '1Au638nEKSAa2xl8WucH_XW3tw8urKooXJHr9kmlkf1E']
+
+const SPREADSHEET_ID = process.argv[2] || sheetIds[0];
 
 (async function () {
     const sheetsFromGoogleDrive = await db.googleSheets.getSheets(SPREADSHEET_ID);
