b4ca4d49aec420fc2e9e16098b57f1c8006fc168
ben willkommen <benwillkommen@gmail.com>
2019-02-17 21:24:02 -0600

    normalized day names

--- src/jobs/clean-rows/util/sheetConsolidation.js
+++ src/jobs/clean-rows/util/sheetConsolidation.js
@@ -27,10 +27,26 @@ function extractDays(cleanedRows) {
     return extractDay(cleanedRows, 1).concat(extractDay(cleanedRows, 2)).concat(extractDay(cleanedRows, 3)).concat(extractDay(cleanedRows, 4));
 }
 
+function normalizeDayName(row){
+    const dayNameLookup = {
+        "monday": "Day 1",
+        "tuesday": "Day 2",
+        "thursday": "Day 3",
+        "friday": "Day 4"
+    }
+    if (dayNameLookup[row[0].toLowerCase()]){
+        const normalizedRow = JSON.parse(JSON.stringify(row));
+        normalizedRow[0] = dayNameLookup[row[0].toLowerCase()]
+        return normalizedRow;
+    }
+    return row;
+}
+
 function consolidateSheet(sheet) {
     const rows = sheet.values;
     const cleanedRows = stripBodyWeightRows(rows.filter(r => !isRowEmpty(r)));
-    const specialCharsRemovedRows = cleanedRows.map(row => row.map(cell => typeof cell === "string" ? cell.replace(/["]*/g, "") : cell));
+    const dayNameNormalizedRows = cleanedRows.map(row => normalizeDayName(row))
+    const specialCharsRemovedRows = dayNameNormalizedRows.map(row => row.map(cell => typeof cell === "string" ? cell.replace(/["]*/g, "") : cell));
     const dayLabeledRows = extractDays(specialCharsRemovedRows, 1);
     const weekLabeledRows = dayLabeledRows.map(r => {
         r.unshift(parseInt(sheet.sheetTitle.replace(/\D/g, '')));
